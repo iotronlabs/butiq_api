ca10796e9087f00059843a8e99dfdf54da1d2603
Akash Kumar Pandey <42707702+Ak-ash13@users.noreply.github.com>
2019-09-29 00:02:39 +0530

    product added

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\ProductIndexResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends ProductIndexResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return array_merge(parent::toArray($request),[
+
+            'variations' => [],
+            
+        ]);
+    }
+}
